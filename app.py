924214be2445c331333e69489f433bb998c725a7
BLQNXAY <3277997863@qq.com>
2019-12-29 14:44:05 +0800

    User authentication with Flask-Login

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, url_for, render_template, redirect, flash, request
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import os
 import click
 
@@ -12,16 +14,31 @@ app.config['SECRET_KEY'] = 'dev'
 
 db = SQLAlchemy(app)
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-class User(db.Model):
+
+class User(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(20))
+	username = db.Column(db.String(20))
+	password_hash = db.Column(db.String(128))
+
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def validate_password(self, password):
+		return check_password_hash(self.password_hash, password)
 
 class Movie(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(60))
 	year = db.Column(db.String(4))
 
+@login_manager.user_loader
+def load_user(user_id):
+	user = User.query.get(int(user_id))
+	return user
 
 @app.cli.command()
 @click.option('--drop', is_flag=True, help='Create after drop')
@@ -58,11 +75,75 @@ def forge():
 	db.session.commit()
 	click.echo('Done')
 
+@app.cli.command()
+@click.option('--username', prompt=True, help='The username used to login')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login')
+def admin(username, password):
+	db.create_all()
+	user = User.query.first()
+	if user is not None:
+		click.echo('Update user...')
+		user.username = username
+		user.set_password(password)
+	else:
+		click.echo('Create user...')
+		user = User(username=username, name='Admin')
+		user.set_password(password)
+		db.session.add(user)
+	
+	db.session.commit()
+	click.echo('Done')
+
 
 @app.context_processor
 def inject_user():
 	user = User.query.first()
 	return dict(user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+
+		if not username or not password:
+			flash('Invalid input')
+			return redirect(url_for('login'))
+
+		user = User.query.first()
+		if user.username == username and user.validate_password(password):
+			login_user(user)
+			flash('Login success')
+			return redirect(url_for('index'))
+		else:
+			flash('Invalid username or password')
+			return redirect(url_for('login'))
+	return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	flash('Goodbye')
+	return redirect(url_for('index'))
+
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+	if request.method == 'POST':
+		name = request.form['name']
+
+		if not name or len(name) > 20:
+			flash('Invalid input')
+			return redirect(url_for('settings'))
+
+		current_user.name = name
+
+		db.session.commit()
+		flash('Settings updated')
+		return redirect(url_for('index'))
+
+	return render_template('settings.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -72,6 +153,9 @@ def page_not_found(e):
 @app.route('/index', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
+		if not current_user.is_authenticated:
+			return redirect(url_for('index'))
+
 		title = request.form.get('title')
 		year = request.form.get('year')
 		if not title or not year or len(year) > 4 or len(title) > 60:
@@ -87,6 +171,7 @@ def index():
 	return render_template('index.html', movies=movies)
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+@login_required
 def edit(movie_id):
 	movie = Movie.query.get_or_404(movie_id)
 	if request.method == 'POST':
@@ -104,6 +189,7 @@ def edit(movie_id):
 
 
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+@login_required
 def delete(movie_id):
 	movie = Movie.query.get_or_404(movie_id)
 	db.session.delete(movie)
