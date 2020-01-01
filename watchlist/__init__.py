29249df00694aaa49b2238a8b80b331f575615e6
BLQNXAY <3277997863@qq.com>
2020-01-01 16:35:21 +0800

    Ready to deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -5,10 +5,11 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, os.getenv('DATABASE_FILE', 'data.db'))
+
 
 db = SQLAlchemy(app)
 
